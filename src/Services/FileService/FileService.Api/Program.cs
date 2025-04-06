fbdb144435819cc45732cce30819a41a66bf3a6f
pofoq <pofoq@mail.ru>
2025-04-06 18:09:43 +0300

    feat: мелочь

--- src/Services/FileService/FileService.Api/Program.cs
+++ src/Services/FileService/FileService.Api/Program.cs
@@ -10,15 +10,13 @@ Console.WriteLine($"Среда выполнения: {environment}");
 
 var appSettings = builder.Configuration.GetSection(nameof(AppSettings)).Get<AppSettings>();
 
-// Подключение к базе данных
-var connectionString = appSettings?.ConnectionString
-           ?? throw new InvalidOperationException("Строка подключения не найдена.");
-
 var services = builder.Services;
 
 services.Configure<AppSettings>(builder.Configuration.GetSection(nameof(AppSettings)));
 
-// Регистрация DbContext
+// Add Database Connection
+var connectionString = appSettings?.ConnectionString
+           ?? throw new InvalidOperationException("Строка подключения не найдена.");
 services.AddDataAccessLayer(connectionString);
 
 // Add services to the container.
@@ -28,6 +26,7 @@ services.AddDomainServices(builder.Configuration.GetSection(nameof(AppSettings))
 services.AddMapperProfiles();
 
 services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 services.AddEndpointsApiExplorer();
 services.AddSwaggerGen();
